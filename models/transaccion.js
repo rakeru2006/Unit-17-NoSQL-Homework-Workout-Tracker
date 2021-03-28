bf5a30504449548f6d8cf44069ae7f5fb471e4aa
rakeru2006 <rakeru2006@gmail.com>
2021-03-27 22:26:01 -0500

    corect route and ading to server

--- models/transaccion.js
+++ models/transaccion.js
@@ -3,42 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema({
-  {
-    day: {
-      type: Date,
-      default: Date.now,
+    {
+        day: {
+        type: Date,
+        default: Date.now,
+        },
+        exercises: [
+        {
+            type: {
+            type: String,
+            trim: true,
+            required: 'Enter an exercise type',
+            },
+            name: {
+            type: String,
+            trim: true,
+            required: 'Enter an exercise name',
+            },
+            duration: {
+            type: Number,
+            required: 'Enter an exercise duration in minutes',
+            },
+            weight: {
+            type: Number,
+            },
+            reps: {
+            type: Number,
+            },
+            sets: {
+            type: Number,
+            },
+            distance: {
+            type: Number,
+            },
+        },
+        ],
     },
-    exercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-          required: 'Enter an exercise type',
-        },
-        name: {
-          type: String,
-          trim: true,
-          required: 'Enter an exercise name',
-        },
-        duration: {
-          type: Number,
-          required: 'Enter an exercise duration in minutes',
-        },
-        weight: {
-          type: Number,
-        },
-        reps: {
-          type: Number,
-        },
-        sets: {
-          type: Number,
-        },
-        distance: {
-          type: Number,
-        },
-      },
-    ],
-  },
 });
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
--- server.js
+++ server.js
@@ -1,46 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const logger = require('morgan');
-//const compression = require('compression');
+const express = require('express');
 
-// Create express server
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.use(logger('dev'));
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'));
-
-
-// compress all responses
-//app.use(compression());
-
-
-// Parse application body as JSON
+app.use(express.static('client'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('./routes/htmlRoutes.js')(app);
 
-// If deployed on heroku, use the deployed database. Otherwise use the local workout database
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}`);
 });
-
-
-
-// routes
-app.use(require('./routes/api.js'));
-//app.use(require('./routes/html-routes'));
-
-
-// routes
-//app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
-
